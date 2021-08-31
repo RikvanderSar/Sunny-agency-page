e8b95309462eb3e081e79ac4240a4f82743d8cb6
rikvandersar <84499256+RikvanderSar@users.noreply.github.com>
2021-08-31 12:16:18 +0200

    add animation to hamburger menu

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 const toggleMenu = document.querySelector('.burger');
 const nav = document.querySelector('nav');
+const hamburgerLine1 = document.querySelector('.line1');
+const hamburgerLine2 = document.querySelector('.line2');
+const hamburgerLine3 = document.querySelector('.line3');
 
 toggleMenu.addEventListener('click', () => {
     nav.classList.toggle('nav--open');
+    hamburgerLine1.classList.toggle('hamburger__transform--line1');
+    hamburgerLine2.classList.toggle('hamburger__transform--line2');
+    hamburgerLine3.classList.toggle('hamburger__transform--line3');
 });
+
+
+
+
